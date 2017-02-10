38afdeb64671b0837999ce65bb9c029ae181bb2d
rtets <ricktetstall@gmail.com>
2017-02-10 22:44:11 +0000

    ho ho ho

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express')
+
+var config = require('./config.json')
+
+var app = express()
+
+//app.use('/api', api);
+app.use('/', express.static(__dirname + '/static')); // redirect root
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect jquery JS
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/react/dist')); // redirect react JS
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/fonts', express.static(__dirname + '/node_modules/bootstrap/dist/fonts')); // redirect fonts bootstrap
+
+app.listen(config.port, function () {
+    console.log('listening on port ' +  config.port)
+})
